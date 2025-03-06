8127269420555be488e2845e18ec76e90a5a84bc
lucasleytao <lucassilvaleitao1401@gmail.com>
2025-03-06 14:47:53 -0300

    feat: Renderização condicional

--- src/components/contador.jsx
+++ src/components/contador.jsx
@@ -4,7 +4,7 @@ import { React, useState } from "react";
 
 export default function Contador() {
     let [contador, setContador] = useState(0)
-    
+
     function aumentar() {
         setContador(contador + 1) //aqui executo outra funcao chamada setContador que define o estado da variavel contador
         // console.log('aumentando', contador) //debug da funcao
@@ -15,16 +15,50 @@ export default function Contador() {
     }
 
     // console.log('renderizando meu contador...') //teste de renderizacao dentro do corpo do componente /debug
+    // return (
+    //     <div>
+    //         <h1>Contador</h1>
+    //         <h3>{contador}</h3> {/*chamo a variavel contador atualizada*/}
+    //         <div style={{margin: '10px 0px 10px 0px'}}>
+    //             {contador === 0 ? 'Vamos começar!' : contador < 0 ? 'Valor negativo' : 'Valor positivo'} {/*operador ternario condicional*/}
+    //         </div>
+    //         <div>
+    //             <button onClick={aumentar}>Aumentar</button> {/* onClick: agarra um evento do navegador e chama a funcao aumentar */}
+    //         </div>
+    //         <div>
+    //             <button onClick={diminuir} style={{ marginTop: '10px' }}>Diminuir</button>
+    //         </div>
+    //     </div>
+    // )
+
+    //CONDICAO DE RETURN *Utilizado geralmente em casos de loadings...
+
+    if (contador > 5) {
+        return (
+            <>
+                <h1>Valor muito alto!</h1>
+                <div>
+                    {/* <div>
+                        <button onClick={aumentar}>Aumentar</button>
+                    </div> */}
+                    <div>
+                        <button onClick={diminuir} style={{ marginTop: '10px' }}>Diminuir</button>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
     return (
-        <div>
+        <>
             <h1>Contador</h1>
-            <h3>{contador}</h3> {/*chamo a variavel contador atualizada*/}
+            <h3>{contador}</h3>
             <div>
-                <button onClick={aumentar}>Aumentar</button> {/* onClick: agarra um evento do navegador e chama a funcao aumentar */}
+                <button onClick={aumentar}>Aumentar</button>
             </div>
             <div>
                 <button onClick={diminuir} style={{ marginTop: '10px' }}>Diminuir</button>
             </div>
-        </div>
+        </>
     )
 }
